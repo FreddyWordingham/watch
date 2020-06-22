b51f92993d5f06448af216b718869427fdfe5d85
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-06-22 22:18:04 +0100

    Updated live site.

--- docs/scripts/blackbox.js
+++ docs/scripts/blackbox.js
@@ -1,5 +1,13 @@
 function transform(word) {
     console.log("Transforming word: ", word);
 
-    return 4324382764329402;
+    word = word.toUpperCase();
+
+    if (word == "FIELD") {
+        console.log("Found field");
+        return 4324382764329402;
+    }
+
+    alert("I don't know that word yet...");
+    return 0;
 }
